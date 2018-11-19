f4ef425246ff38a9c1728bcba319db2e48abb7c7
Jelle <geers.jelle@gmail.com>
2018-11-19 15:51:23 +0100

    update

---
+++ BreakOutGame/Models/Domain/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BreakOutGame.Models.Domain
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String ClassNumber { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        [JsonIgnore]
+        public StudentClass StudentClass { get; set; }
+        [JsonIgnore]
+        public IEnumerable<GroupStudent> Groups { get; set; }
+    }
+}
---
+++ BreakOutGameTest/Controllers/SessionTest.cs
@@ -0,0 +1,75 @@
+﻿using Xunit;
+using Moq;
+using BreakOutGameTest.Data;
+using BreakOutGame.Models.Domain.RepsitoryInterfaces;
+using BreakOutGame.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace BreakOutGameTest.Controllers
+{
+    public class SessionTest
+    {
+        private readonly DummyApplicationDbContext _dummyContext;
+        private readonly Mock<IBoBSessionRepository> _sessionRepository;
+        private readonly SessionController _controller;
+
+        public SessionTest()
+        {
+            this._dummyContext = new DummyApplicationDbContext();
+            this._sessionRepository = new Mock<IBoBSessionRepository>();
+            this._controller = new SessionController(_sessionRepository.Object) ;
+        }
+
+        #region tests
+
+        [Fact]
+        public void SessionCode_Valid_RedirectsTo_BoBGroup_Index()
+        {
+            //trek een id binnen, get sessie met id, check of deze sessie actief is
+            _sessionRepository.Setup(s => s.GetById(1)).Returns(_dummyContext.ValidSession);
+            var result = _controller.ActivateSession(1) as RedirectToActionResult;
+
+            Assert.Equal("BoBGroup", result?.ControllerName);
+            Assert.Equal("Index", result?.ActionName);
+        }
+
+
+        [Fact]
+        public void SessionCode_ClosedSession_RedirectBackTo_SessionCodeScreen()
+        {
+            _sessionRepository.Setup(s => s.GetById(2)).Returns(_dummyContext.ClosedSession);
+            var result = _controller.ActivateSession(2) as RedirectToActionResult;
+
+            Assert.Equal("Session", result?.ControllerName);
+            Assert.Equal("Index", result?.ActionName);
+        }
+
+        [Fact]
+        public void SessionCode_ActiveSession_RedirectBackTo_SessionCodeScreen()
+        {
+            _sessionRepository.Setup(s => s.GetById(3)).Returns(_dummyContext.ActiveSession);
+            var result = _controller.ActivateSession(3) as RedirectToActionResult;
+
+            Assert.Equal("Session", result?.ControllerName);
+            Assert.Equal("Index", result?.ActionName);
+        }
+
+        [Fact]
+        public void SessionCode_StartedSession_RedirectBackTo_SessionCodeScreen()
+        {
+            _sessionRepository.Setup(s => s.GetById(4)).Returns(_dummyContext.StartedSession);
+            var result = _controller.ActivateSession(4) as RedirectToActionResult;
+
+            Assert.Equal("Session", result?.ControllerName);
+            Assert.Equal("Index", result?.ActionName);
+        }
+
+        #endregion
+    }
+}
+
+
+
+
